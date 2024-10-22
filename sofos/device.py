4048d6ca27320a4d73be7cead69bede48c783123
PierreSavatte <pierre-savatte@laposte.net>
2024-10-22 12:20:09 +0200

    add log to know which device is used

--- sofos/device.py
+++ sofos/device.py
@@ -2,8 +2,10 @@ import torch
 
 
 def get_device():
-    return torch.device(
+    device_name = (
         "cuda"
         if torch.cuda.is_available()  # If GPU is to be used
         else "mps" if torch.backends.mps.is_available() else "cpu"
     )
+    print(f"Using {device_name=}")
+    return torch.device(device_name)
