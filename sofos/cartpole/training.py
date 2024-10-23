e68bd80b88beb3c085b69797c8fa34576054f0af
PierreSavatte <pierre-savatte@laposte.net>
2024-10-23 15:24:54 +0200

    improve documentation on variable names

--- sofos/cartpole/training.py
+++ sofos/cartpole/training.py
@@ -131,9 +131,8 @@ class Trainer:
         # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
         # columns of actions taken. These are the actions which would've been
         # taken for each batch state according to policy_net
-        state_action_values = self.policy_net(state_batch).gather(
-            1, action_batch
-        )
+        policy_results = self.policy_net(state_batch)
+        state_action_values = policy_results.gather(1, action_batch)
 
         # Compute V(s_{t+1}) for all next states.
         # Expected values of actions for non_final_next_states are computed
